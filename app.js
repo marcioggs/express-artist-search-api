94c4a8329d856a291b42a4e9085ccb091285c8ad
Márcio Gabriel Gomes da Silva <marcioggs@gmail.com>
2018-03-13 19:11:29 -0300

    Removed unused files and renamed the project.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -13,8 +12,6 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/artist', artist);
 
